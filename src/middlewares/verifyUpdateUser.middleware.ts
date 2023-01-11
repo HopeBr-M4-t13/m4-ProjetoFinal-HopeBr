2d915e3eaa7b3d02e6c2e458043db02e98714eff
Silas Junior <silas.mcoach@gmail.com>
2023-01-11 17:25:40 -0300

    feat: Add middleware verify update user

---
+++ src/middlewares/verifyUpdateUser.middleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import AppError from "../errors/AppError";
+
+
+
+export const verifyUpdateUserMiddleware = async(req: Request, res: Response, next: NextFunction) =>{
+
+    const setBody = req.body
+
+    const dataKeys = Object.keys(setBody)
+
+    if(dataKeys.includes("isAdm") || dataKeys.includes("isActive") || dataKeys.includes("id")){
+        throw new AppError('it is not allowed to edit the fields: isActive, IsAdm and id', 401)
+    }
+
+    return next();
+}
