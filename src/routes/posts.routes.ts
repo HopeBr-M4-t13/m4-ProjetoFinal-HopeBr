738afad135f3674ee5850329813794f3b023900b
Isaias Gregory <106548529+Lightgreg@users.noreply.github.com>
2023-01-13 15:02:11 -0300

    Update posts.routes.ts

--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -1,12 +1,13 @@
 import {Router} from "express"
 import { createPostController, deletePostController, listPostController, listUniquePostController, updatePostController } from "../controllers/posts/posts.controller";
 import verifyAuthMiddleware from "../middlewares/verifyAuth.middleware";
+import verifyDataMiddleware from "../middlewares/verifyData.middleware";
 import ensureOwnerOrAdminMiddleware from "../middlewares/verifyOwnerOrAdmin.middleware";
 import { createPostSerializer } from "../serializers/post.serializer";
 
 const postRoutes = Router();
 
-postRoutes.post("", verifyAuthMiddleware, validateData(createPostSerializer), createPostController);
+postRoutes.post("", verifyAuthMiddleware, verifyDataMiddleware(createPostSerializer), createPostController);
 postRoutes.get("", verifyAuthMiddleware, listPostController);
 postRoutes.get("/:id", verifyAuthMiddleware, listUniquePostController);
 postRoutes.patch("/:id", verifyAuthMiddleware, ensureOwnerOrAdminMiddleware, updatePostController);
