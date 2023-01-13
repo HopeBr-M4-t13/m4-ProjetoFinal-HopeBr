d70f8e8fbd38d28c72b6c1f6410895a8b31136e4
Isaias Gregory <106548529+Lightgreg@users.noreply.github.com>
2023-01-13 16:24:54 -0300

    feat/createDbOnline

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,28 +1,48 @@
-import 'reflect-metadata'
-import 'dotenv/config'
-import path from 'path'
-import { DataSource } from 'typeorm'
+import "reflect-metadata";
+import "dotenv/config";
+import path from "path";
+import { DataSource, DataSourceOptions } from "typeorm";
 
-const AppDataSource = new DataSource(
-    process.env.NODE_ENV === "test" ?
-    {
-        type: "sqlite",
-        database: ":memory:",
-        synchronize: true,
-        entities: ["src/entities/*.ts"]
-    } :
-    {
+const setDataSourceConfig = (): DataSourceOptions => {
+    const entitiesPath: string = path.join(__dirname, "./entities/**.{js,ts}");
+    const migrationsPath: string = path.join(
+        __dirname,
+        "./migrations/**.{js,ts}"
+    );
+
+    const nodeEnv = process.env.NODE_ENV;
+
+    if (nodeEnv === "production") {
+        return {
+            type: "postgres",
+            url: process.env.DATABASE_URL,
+            entities: [entitiesPath],
+            migrations: [migrationsPath],
+        }
+    }
+
+    if (nodeEnv === "test") {
+        return {
+            type: "sqlite",
+            database: ":memory:",
+            synchronize: true,
+            entities: [entitiesPath],
+        };
+    }
+
+    return {
         type: "postgres",
         host: process.env.PGHOST,
-        port: parseInt(process.env.PGPORT!),
         username: process.env.PGUSER,
         password: process.env.PGPASSWORD,
-        database: process.env.PGDATABASE,
-        logging: true,
+        port: parseInt(process.env.PGPORT),
+        database: process.env.DB,
         synchronize: false,
-        entities: [path.join(__dirname, "./entities/**.{js,ts}")],
-        migrations: [path.join(__dirname, "./migrations/**.{js,ts}")]
-    }
-)
+        logging: true,
+        entities: [entitiesPath],
+        migrations: [migrationsPath],
+    };
+};
 
-export default AppDataSource
+const dataSourceConfig = setDataSourceConfig();
+export default new DataSource(dataSourceConfig);
