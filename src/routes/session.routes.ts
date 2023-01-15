cd3d282c9870c9122508062516178546f48cf49a
Pedro Mendes <106615792+pedroMendes182@users.noreply.github.com>
2023-01-15 16:57:54 -0300

    feat: add virify user is active middleware

--- src/routes/session.routes.ts
+++ src/routes/session.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import sessionUserController from "../controllers/session/session.controller";
 import verifyDataMiddleware from "../middlewares/verifyData.middleware";
+import verifyUserIsActiveMiddleware from "../middlewares/verifyUserIsActive.middleware";
 import sessionSerializer from "../serializers/session.serializers";
 
 const sessionRoutes = Router();
 
-sessionRoutes.post("", verifyDataMiddleware(sessionSerializer), sessionUserController);
+sessionRoutes.post("", verifyDataMiddleware(sessionSerializer), verifyUserIsActiveMiddleware, sessionUserController);
 
 export default sessionRoutes;
